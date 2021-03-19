71fa0624d8a9451193f38f6f414bb2bf17b886ca
Charity Mutoni <charitytoni0@gmail.com>
2021-03-19 19:26:44 +0200

    created class news_sources

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class news_source:
+    '''
+    news_source class to define news_Source objects
+    '''
+   def __init__(self,id,name,url,description,category):
+        self.id = id
+        self.name = name
+        self.url = url
+        self.description = description
+        self.category =category
+
