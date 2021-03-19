425e48dce2c224def21f65039ceb97a3a996d074
Charity Mutoni <charitytoni0@gmail.com>
2021-03-19 20:57:13 +0200

    made changes

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 class News_source:
     '''
-    news_source class to define news_Source objects
+    Class for defining Source objects
     '''
-   def __init__(self,id,name,url,description,category):
+    def __init__(self,id,name,url,description,category):
+        '''
+        Method for defining the news variables
+        '''
         self.id = id
         self.name = name
         self.url = url
         self.description = description
         self.category =category
-
+        
+        
