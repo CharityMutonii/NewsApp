d22efb5a1cffe62f1585248c31dd063a903c1d8c
Charity Mutoni <charitytoni0@gmail.com>
2021-03-19 21:39:24 +0200

    added my manage.py

--- app/request.py
+++ app/request.py
@@ -15,7 +15,7 @@ def get_sources(category):
     '''
     This function gets response from the news site
     '''
-    get_sources_url = base_url.format(category,api_key)
+    get_sources_url = source_url.format(category,api_key)
 
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
